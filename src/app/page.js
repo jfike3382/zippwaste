468350153d2accda6e68d742770eae506b763de2
Vagan <vagan.abel@gmail.com>
2025-08-24 16:46:55 +0300

    backend setup

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="text-4xl font-bold">This is Zippwaste Project</h1>
+      <main className="flex flex-col gap-[32px] row-start-2 items-center">
+        <Image
+          src="/zippwaste-logo.svg"
+          alt="Zippwaste Logo"
+          width={200}
+          height={100}
+        />
+        <h1 className="text-4xl font-bold">Project launch soon</h1>
       </main>
     </div>
   );
