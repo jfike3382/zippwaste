4c410a00cbfcfed80e6fd844a150243e86fe1340
Vagan <vagan.abel@gmail.com>
2025-09-01 15:23:12 +0300

    data api client.js

---
+++ src/api/data-client.js
@@ -0,0 +1,19 @@
+import createApiClient from "./api-request";
+
+const API_BASE_URL = process.env.NEXT_PUBLIC_DATA_API_BASE_URL;
+const apiRequest = createApiClient(API_BASE_URL);
+
+export const SoloApi = {
+  company: (slug) => {
+    const headers = {
+      "X-Secret-Key": process.env.CRON_SECRET_KEY,
+    };
+    return apiRequest(`solo/company/${slug}`, "GET", null, false, headers);
+  },
+};
+
+export const TableApi = {
+  companies: (filters = {}) => {
+    return apiRequest("table/companies", "POST", filters, false);
+  },
+};
