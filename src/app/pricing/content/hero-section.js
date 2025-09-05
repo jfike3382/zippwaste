bb2cffc2898e7464d119536b976e22b627d2f6a2
Vagan <vagan.abel@gmail.com>
2025-09-05 13:42:16 +0300

    hero descriptions

--- src/app/pricing/content/hero-section.js
+++ src/app/pricing/content/hero-section.js
@@ -1,22 +1,12 @@
 import PlansContainer from "./plans-container";
-import RaizerLogoIcon from "@/uikit/icons/raizer-icon";
 
 export default function FirstScreen() {
   return (
     <section className="flex flex-col gap-6 w-full">
-      <div className="flex flex-col justify-center items-center text-center gap-6">
-        <h1 className="title-xl max-w-4xl inline-flex items-center gap-3 max-xl:block">
-          Raise with confidence{" "}
-          <span className="text-brand-violet-500 tablet-hidden mobile-hidden">
-            <RaizerLogoIcon size={38} />
-          </span>
-          <span className="violet-gradient-text inline-flex items-center gap-2">
-            and AI
-          </span>
-        </h1>
-        <p className="paragraph-xl max-w-lg">
-          Friendly pricing designed for every founder. Start today, no credit
-          card required.
+      <div className="flex flex-col justify-center items-center text-center gap-6 max-w-xl mx-auto">
+        <h1 className="title-xl ">Pricing</h1>
+        <p className="paragraph-xl flex flex-col gap-1">
+          <span>Listing plans designed for businesses of all sizes.</span> <span>Start today. Simple and transparent.</span>
         </p>
       </div>
       <PlansContainer />
