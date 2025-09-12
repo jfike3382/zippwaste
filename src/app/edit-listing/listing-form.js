0b0b381848d24a07cf8377e2d61b0dabd38cd7a5
Vagan <vagan.abel@gmail.com>
2025-09-12 19:07:29 +0300

    save for later button implemented

--- src/app/edit-listing/listing-form.js
+++ src/app/edit-listing/listing-form.js
@@ -1,4 +1,5 @@
 import Input from "@/uikit/input";
+import Button from "@/uikit/button";
 
 import LogoUploader from "@/utils/logo-uploader";
 import ProfileLogo from "@/uikit/profile-logo";
@@ -6,21 +7,27 @@ import Selector from "@/uikit/selector";
 import filtersData from "@/data/filters.json";
 import Image from "next/image";
 import { useState } from "react";
+import { ListingApi } from "@/api/actions-client";
+import { useNotification } from "@/providers/notifications";
 import {
   useInputChangeHandler,
   useLogoChangeHandler,
   useSelectorChangeHandler,
-  useGetDataToSave,
 } from "./form-handlers";
 
 export default function Section({ onSectionChange, data, onFormDataChange }) {
   const name = data?.name || "";
   const logo = data?.logo || null;
-  const tagline = data?.tagline || "";
   const description = data?.description || "";
+  const address = data?.address || "";
+  const city = data?.city || "";
+  const state = data?.state || "";
   const debris_type = data?.debris_type || [];
   const website = data?.website || "";
   const x_account = data?.x_account || "";
+
+  const [saving, setSaving] = useState(false);
+  const { showNotification } = useNotification();
 
   const handleInputChange = useInputChangeHandler(null, data, onFormDataChange);
   const handleLogoChange = useLogoChangeHandler(null, data, onFormDataChange);
@@ -28,13 +35,6 @@ export default function Section({ onSectionChange, data, onFormDataChange }) {
     null,
     data,
     onFormDataChange
-  );
-
-  const getDataToSave = useGetDataToSave(
-    null,
-    ["name", "logo", "tagline", "description", "debris_type", "website"],
-    data,
-    { sectionFillKey: "section_1_fill", sectionStep: 1 }
   );
 
   const removeAppliedDebrisType = (debrisType) => {
@@ -46,21 +46,45 @@ export default function Section({ onSectionChange, data, onFormDataChange }) {
     (industry) => industry.name
   );
 
-  const [taglineValue, setTaglineValue] = useState(tagline);
-
-  const handleTaglineChange = (e) => {
-    const value = e.target.value.slice(0, 100);
-    setTaglineValue(value);
-    handleInputChange({
-      target: {
-        name: "tagline",
-        value,
-      },
-    });
+  const handleSaveForLater = async () => {
+    setSaving(true);
+    try {
+      const response = await ListingApi.editCompanyInfo(data);
+      if (!response.error) {
+        showNotification("success", response.success_message);
+      } else {
+        showNotification("error", response.error);
+      }
+    } catch (error) {
+      showNotification("error", error.message);
+    } finally {
+      setSaving(false);
+    }
   };
 
   return (
     <>
+      <section className="flex flex-col gap-4">
+        <div className="flex items-center justify-between">
+          <h1 className="title-m">Claim your business</h1>
+          <div className="w-36">
+            <Button
+              variant="secondary"
+              size="m"
+              onClick={handleSaveForLater}
+              loading={saving}
+              fullWidth
+            >
+              Save for later
+            </Button>
+          </div>
+        </div>
+        <p className="paragraph-l">
+          {" "}
+          Share the core information about your company
+        </p>
+      </section>
+      <div className="divider" />
       <div className="flex flex-col gap-8 items-start">
         <ProfileLogo name={name} size="l" src={logo || null} placeholder="🖼" />
         <LogoUploader
@@ -80,19 +104,6 @@ export default function Section({ onSectionChange, data, onFormDataChange }) {
             required
           />
         </div>
-        <div className="relative">
-          <Input
-            name="tagline"
-            label="Tagline"
-            value={taglineValue}
-            onChange={handleTaglineChange}
-            placeholder="Concise and descriptive tagline"
-            required
-          />
-          <div className="absolute bg-white pl-2 right-3 mt-4.5 top-1/2 -translate-y-1/2 text-sm font-semibold text-right">
-            {taglineValue.length}/100
-          </div>
-        </div>
         <Input
           type="textarea"
           name="description"
@@ -100,6 +111,30 @@ export default function Section({ onSectionChange, data, onFormDataChange }) {
           value={description}
           onChange={handleInputChange}
           placeholder="What's your company about?"
+          required
+        />
+        <Input
+          name="address"
+          label="Full address"
+          value={address}
+          onChange={handleInputChange}
+          placeholder="Enter your full address"
+          required
+        />
+        <Input
+          name="city"
+          label="City"
+          value={city}
+          onChange={handleInputChange}
+          placeholder="Enter your city"
+          required
+        />
+        <Input
+          name="state"
+          label="State"
+          value={state}
+          onChange={handleInputChange}
+          placeholder="Enter your state"
           required
         />
         <div className="flex flex-col gap-4">
--- src/app/edit-listing/page-wrapper.js
+++ src/app/edit-listing/page-wrapper.js
@@ -3,7 +3,7 @@ import { useState, useEffect, useCallback } from "react";
 import GlobalLoader from "@/components/global-elements/global-loader";
 import { ListingApi } from "@/api/actions-client";
 
-import MainInfo from "./main-info";
+import ListingForm from "./listing-form";
 
 export default function PageWrapper() {
   const [formData, setFormData] = useState(null);
@@ -33,26 +33,17 @@ export default function PageWrapper() {
     <>
       <GlobalLoader show={loading} />
       <div className="min-h-screen">
-          {!loading && (
-            <div className="main-data-container">
-              <div className="card-container flex flex-col gap-8 p-8 max-md:px-4">
-                <section className="flex flex-col gap-4">
-                  <h1 className="title-m">Claim your business</h1>
-                  <p className="paragraph-l">
-                    {" "}
-                    Share the core information about your company
-                  </p>
-                </section>
-                <div className="divider" />
-                <MainInfo
-                  data={formData}
-                  onFormDataChange={handleFormDataChange}
-                />
-              </div>
+        {!loading && (
+          <div className="main-data-container">
+            <div className="card-container flex flex-col gap-8 p-8 max-md:px-4">
+              <ListingForm
+                data={formData}
+                onFormDataChange={handleFormDataChange}
+              />
             </div>
-          )}
-        </div>
- 
+          </div>
+        )}
+      </div>
     </>
   );
 }
