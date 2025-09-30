118fa7512070c168ee59159a909320ea524bf050
Vagan <vagan.abel@gmail.com>
2025-09-30 10:59:18 +0300

    filter for blog posts mobile

--- src/app/blog/page-wrapper.js
+++ src/app/blog/page-wrapper.js
@@ -3,9 +3,27 @@
 import { useState } from "react";
 import BlogFilter from "./blog-filter";
 import BlogItem from "./blog-item";
+import Selector from "@/uikit/selector";
 
 export default function PageWrapper({ posts }) {
   const [displayedPosts, setDisplayedPosts] = useState(posts);
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  // Get unique categories from posts
+  const categories = [...new Set(posts.map((post) => post.category))];
+  const categoryOptions = [
+    { label: "All posts", value: "" },
+    ...categories.map((cat) => ({ label: cat, value: cat })),
+  ];
+
+  const handleMobileFilterChange = (category) => {
+    setSelectedCategory(category);
+    if (category === "" || category === "All posts") {
+      setDisplayedPosts(posts);
+    } else {
+      setDisplayedPosts(posts.filter((post) => post.category === category));
+    }
+  };
 
   return (
     <div className="flex flex-row flex-1 ">
@@ -14,11 +32,20 @@ export default function PageWrapper({ posts }) {
       </aside>
       <div className="main-data-right-container">
         <div className="flex flex-col gap-4">
-          <p className="title-l">Blog</p>{" "}
+          <p className="title-l">Blog</p>
           <p className="paragraph-l">
             News, posts, insights, and tips from Zippwaste team
           </p>
+          <div className="desktop-hidden">
+            <Selector
+              value={selectedCategory}
+              onChange={handleMobileFilterChange}
+              options={categoryOptions}
+              placeholder="All posts"
+            />
+          </div>
         </div>
+
         {displayedPosts.map((blogpost, index) => (
           <BlogItem key={index} blogpost={blogpost} />
         ))}
