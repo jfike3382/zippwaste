92ecb90d0fed4568951e02f96bbba2a58d05f3a6
Vagan <vagan.abel@gmail.com>
2025-10-03 12:03:01 +0300

    terms embed

--- src/app/terms-of-use/page-wrapper.js
+++ src/app/terms-of-use/page-wrapper.js
@@ -1,6 +1,15 @@
-import Content from "./content";
+"use client";
+
+import { useEffect } from "react";
 
 export default function PageWrapper() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src =
+      "https://policies.termageddon.com/api/embed/WVRseVdGUlhiR1pTZWxSMmNsRTlQUT09.js";
+    document.head.appendChild(script);
+  }, []);
+
   return (
     <div className="main-data-container">
       <div className="card-container flex flex-col gap-8 p-8 max-md:px-4">
@@ -9,7 +18,20 @@ export default function PageWrapper() {
           <p className="paragraph-l">Effective Date: Sep 8, 2025</p>
         </section>
         <div className="divider" />
-        <Content />
+        <div
+          id="WVRseVdGUlhiR1pTZWxSMmNsRTlQUT09"
+          className="policy_embed_div"
+        >
+          Please wait while the policy is loaded. If it does not load, please{" "}
+          <a
+            rel="nofollow"
+            href="https://policies.termageddon.com/api/policy/WVRseVdGUlhiR1pTZWxSMmNsRTlQUT09"
+            target="_blank"
+          >
+            click here
+          </a>{" "}
+          to view the policy.
+        </div>
       </div>
     </div>
   );
